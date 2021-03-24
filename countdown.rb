35e905169b5713a87c9f8d5789e036c24ca9eea9
+ <+>
2021-03-24 13:12:57 -0700

    Create new years countdown timer

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
+def countdown(start_number)
 
-def countdown
+    while 0 < start_number
+        puts "#{start_number} SECOND(S)!\n" if start_number > 0
+        start_number -= 1
+        sleep(1)
+        return "HAPPY NEW YEAR!" if start_number == 0
+    end
+
+end
+
+def countdown_with_sleep(start_number)
+
+    while 0 < start_number
+        puts "#{start_number} SECOND(S)!\n" if start_number > 0
+        start_number -= 1
+        sleep(1)
+        return "HAPPY NEW YEAR!" if start_number == 0
+    end
+
 end
